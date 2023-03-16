4c60560710c95200dd077ec70b97b187053fbece
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2023-03-16 15:09:15 -0300

    work section updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -12,40 +12,7 @@ const Work = () => {
             <h2>Some Things I've built</h2>
             <hr/>
         </div>
-
-        <Project
-        textAlign='end'
-        alignItems='flex-end'
-        src={SimpleCalculator}
-        title='Calculator'
-        type='Personal Project'
-        info={<><p>A simple calculator using only HTML, CSS and JAVASCRIPT.</p> <p>I started by creating the basic HTML structure and using CSS to style the calculator's layout. I then implemented JavaScript to add functionality to the calculator, allowing users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.</p> <p>Overall, creating this calculator was a great opportunity for me to practice.</p></>}
-        techs={
-                <>
-                <li>VS Code</li>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                </>
-              }
-        />
-        <Project
-        direction='row-reverse'
-        alignItems='flex-start'
-        src={SimonGame}
-        title='Simon Game'
-        type='Academic Project'
-        info='A "Simon Says" game with HTML, CSS and JS'
-        techs={
-            <>
-            <li>VS Code</li>
-            <li>HTML</li>
-            <li>Bootstrap</li>
-            <li>JQuery</li>
-            <li>JavaScript</li>
-            </>
-        }
-        />
+        
         <Project
         textAlign='end'
         alignItems='flex-end'
@@ -63,6 +30,42 @@ const Work = () => {
                 </>
               }
         />
+        
+        <Project
+        direction='row-reverse'
+        alignItems='flex-start'
+        src={SimonGame}
+        title='Simon Game'
+        type='Academic Project'
+        info='A "Simon Says" game with HTML, CSS and JS'
+        techs={
+            <>
+            <li>VS Code</li>
+            <li>HTML</li>
+            <li>Bootstrap</li>
+            <li>JQuery</li>
+            <li>JavaScript</li>
+            </>
+        }
+        />
+
+        <Project
+        textAlign='end'
+        alignItems='flex-end'
+        src={SimpleCalculator}
+        title='Calculator'
+        type='Personal Project'
+        info={<><p>A simple calculator using only HTML, CSS and JAVASCRIPT.</p> <p>I started by creating the basic HTML structure and using CSS to style the calculator's layout. I then implemented JavaScript to add functionality to the calculator, allowing users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.</p> <p>Overall, creating this calculator was a great opportunity for me to practice.</p></>}
+        techs={
+                <>
+                <li>VS Code</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                </>
+              }
+        />
+        
 
     </div>
   )
