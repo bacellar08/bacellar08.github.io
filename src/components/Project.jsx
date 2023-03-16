91ec4f7e3c3ae2d39d3191f1873ec55e05d41eef
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2023-03-16 15:04:07 -0300

    Work section update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,20 +1,23 @@
-import { GitHub } from '@mui/icons-material'
+import { GitHub, Launch } from '@mui/icons-material'
 import '../styles/project.css'
 
 const Project = (p) => {
   return (
     <div className='project-container' style={{flexDirection: p.direction}}>
-        <div className="img-space" style={{height: "300px", width: "400px"}}>
+        <div className="img-space" style={{height: "300px", width: "450px"}}>
         </div>
         <img className='project-img' src={p.src} alt="Example" />
         <div className="project-info" style={{alignItems: p.alignItems}}>
             <span className='project-type'>{p.type}</span>
             <h3 className='project-title'>{p.title}</h3>
-            <p className='project-description'>{p.info}</p>
+            <div className='project-description' style={{textAlign: p.textAlign}}>{p.info}</div>
             <div className="project-techs">
                 {p.techs}
             </div>
+            <div className='project-icons'>
             <i style={{color: 'var(--title-Color)', paddingTop: '20px'}}><GitHub/></i>
+            <i style={{color: 'var(--title-Color)', paddingTop: '20px'}}><Launch/></i>
+            </div>
         </div>
     </div>
   )
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import '../styles/work.css'
 import Project from './Project'
 import SimpleCalculator from '../img/simple-calculator.png'
 import SimonGame from '../img/simon-game.png'
+import TinDog from '../img/tindog.png'
 
 const Work = () => {
   return (
@@ -13,11 +14,12 @@ const Work = () => {
         </div>
 
         <Project
+        textAlign='end'
         alignItems='flex-end'
         src={SimpleCalculator}
         title='Calculator'
         type='Personal Project'
-        info='A simple calculator using only HTML, CSS and JAVASCRIPT'
+        info={<><p>A simple calculator using only HTML, CSS and JAVASCRIPT.</p> <p>I started by creating the basic HTML structure and using CSS to style the calculator's layout. I then implemented JavaScript to add functionality to the calculator, allowing users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.</p> <p>Overall, creating this calculator was a great opportunity for me to practice.</p></>}
         techs={
                 <>
                 <li>VS Code</li>
@@ -25,7 +27,7 @@ const Work = () => {
                 <li>CSS</li>
                 <li>JavaScript</li>
                 </>
-            }
+              }
         />
         <Project
         direction='row-reverse'
@@ -44,6 +46,24 @@ const Work = () => {
             </>
         }
         />
+        <Project
+        textAlign='end'
+        alignItems='flex-end'
+        src={TinDog}
+        title='TinDog'
+        type='Academic Project'
+        info={<><p>TinDog, a fictional app that serves as a "tinder for dogs," was an exciting and creative project.</p> <p>The website's design incorporated playful and colorful elements that appeal to both dogs and their owners. I used HTML, CSS, and JavaScript to create an interactive and user-friendly interface, making it easy for users to learn about the app's features and download it aswell as Bootstrap to ensure that the website was responsive and mobile-friendly.</p></>}
+        techs={
+                <>
+                <li>VS Code</li>
+                <li>HTML</li>
+                <li>Bootstrap</li>
+                <li>JQuery</li>
+                <li>JavaScript</li>
+                </>
+              }
+        />
+
     </div>
   )
 }
