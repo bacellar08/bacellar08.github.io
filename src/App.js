a833184ae077603667aa3b06ade101e71ed2965a
Alexandre Bacellar <alexandre.bacellar@gmail.com>
2023-03-20 21:21:17 -0300

    responsive fixe wip

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
 
-    <div style={{background: '#0A192F', height: '100vh'}}>
+    <div className='portfolio' style={{background: '#0A192F', height: '100vh'}}>
       <Social/>
       <Nav  item1='about' item2='experience' item3='work' item4='contact'/>
       <Introduction/>
